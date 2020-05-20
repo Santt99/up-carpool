f69b75b97b64ddd1bff00de79743d5a5934d0a59
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2020-05-20 00:15:43 -0500

    Added: Carousel

--- app.py
+++ app.py
@@ -4,36 +4,35 @@ from json import dumps
 from flask_cors import CORS
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static", static_folder="/static")
 
 CORS(app)
-DB_URL = "mongodb://{}:{}@ds159328.mlab.com:59328/heroku_4g6p2nzn".format("front", "Teclado12M")
+DB_URL = "mongodb://{}:{}@ds159328.mlab.com:59328/heroku_4g6p2nzn".format(
+    "front", "Teclado12M"
+)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     res = make_response(render_template("index.html"))
-    if not request.cookies.get('visitId'):
+    if not request.cookies.get("visitId"):
         db = DB(DB_URL)
         visit_id = db.register_visit()
-        res.set_cookie('visitId', str(visit_id))
+        res.set_cookie("visitId", str(visit_id))
     return res
 
-@app.route('/index.js')
-def get_index_js():
-    print("Hi")
-    return app.send_static_file("/templates/index.js")
 
-
-@app.route('/updatevisit/<email>')
+@app.route("/updatevisit/<email>")
 def update_visit(email):
     from datetime import datetime
+
     db = DB(DB_URL)
     data = {"$set": {"second_timestamp": datetime.now(), "email": email}}
-    print(request.cookies.get('visitId'))
-    key = {"_id": request.cookies.get('visitId')}
+    print(request.cookies.get("visitId"))
+    key = {"_id": request.cookies.get("visitId")}
     db.update_visit(key, data)
     return dumps({"message": "Succesfully updated visit"})
 
-if __name__ == '__main__':
-    app.run(port=3000)
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
